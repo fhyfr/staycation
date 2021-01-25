47cd1dac34a67298138b2fedebc0319492744bf0
Firman <fhaya.firman@gmail.com>
2021-01-25 11:59:44 +0700

    create section categories view

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
+import Categories from 'parts/Categories'
+
 import landingPage from 'json/landingPage'
 
 export default class LandingPage extends Component {
@@ -18,6 +20,7 @@ export default class LandingPage extends Component {
 
             <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} /> 
             <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
+            <Categories data={landingPage.categories} />
         </>
         );
     }
