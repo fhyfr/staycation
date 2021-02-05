d892f6b3aa935ee9bcd68dabd9cb3b655fa3ca6c
Firman <fhaya.firman@gmail.com>
2021-02-05 09:52:52 +0700

    create input number component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import LandingPage from 'pages/LandingPage';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './assets/scss/style.scss';
+import Example from 'pages/Example';
 
 
 function App() {
